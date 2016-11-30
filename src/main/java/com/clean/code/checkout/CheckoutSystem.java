5bc5ca406f059f1410ff6db18b81f5759a3566c9
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-11-30 18:43:45 +0530

    Commit 9 - GREEN (Added Implementation to add the prices of 1 Unit of A and B Items at checkout).

--- src/main/java/com/clean/code/checkout/CheckoutSystem.java
+++ src/main/java/com/clean/code/checkout/CheckoutSystem.java
@@ -7,15 +7,17 @@ import java.util.Map;
 public class CheckoutSystem {
 
 	public double calculateTotatlPrice(List<String> itemsForBillingAtCheckout) {
-		Double zeroPrice = Double.valueOf(0.0);
+		Double totalItemPrice = Double.valueOf(0.0);
 		Map<String, Double> itemPriceMap = new HashMap<>();
 		itemPriceMap.put("A", Double.valueOf(50.0));
 		itemPriceMap.put("B", Double.valueOf(30.0));
 		
-		if(itemsForBillingAtCheckout != null && itemsForBillingAtCheckout.size() == 1){
-			return itemPriceMap.getOrDefault(itemsForBillingAtCheckout.get(0), zeroPrice);			
+		if(itemsForBillingAtCheckout != null){
+			for(String item : itemsForBillingAtCheckout){
+				totalItemPrice += itemPriceMap.getOrDefault(item, totalItemPrice);
+			}			
 		}			
-		return zeroPrice;
+		return totalItemPrice;
 	}
 
 }
