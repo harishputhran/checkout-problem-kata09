3ed78f72a7e62175a4e90ec02cc32f30c5333300
Puthran, Harish <Harish.Puthran@in.tesco.com>
2016-11-30 09:45:08 +0000

    Commit 4 - RED (Added Test Case when One Unit of Item A is available at Checkout)

--- src/test/java/com/clean/code/checkout/CheckoutSystemShould.java
+++ src/test/java/com/clean/code/checkout/CheckoutSystemShould.java
@@ -2,6 +2,9 @@ package com.clean.code.checkout;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -10,9 +13,16 @@ import org.junit.runners.JUnit4;
 public class CheckoutSystemShould {	
 	
 	@Test
-	public void return_0_When_No_Items_During_Checkout(){
+	public void return_price_as_0_when_no_items_at_checkout(){
 		CheckoutSystem checkout = new CheckoutSystem();
 		assertEquals(0.0d, checkout.calculateTotatlPrice(null), 0.0d);
+	}
+	
+	@Test
+	public void return_price_of_50__for_one_unit_of_itemA_at_checkout(){
+		CheckoutSystem checkout = new CheckoutSystem();
+		List<String> items = Arrays.asList("A");
+		assertEquals(50.0d, checkout.calculateTotatlPrice(items), 0.0d);
 	}
 
 }
