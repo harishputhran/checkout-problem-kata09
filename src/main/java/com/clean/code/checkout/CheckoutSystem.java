2204673caff9a6e6ee851befb5fcc87c763caa76
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-11-30 19:10:13 +0530

    Commit 11 - GREEN (Implementation of Discounted Price for 3 Item A at checkout)

--- src/main/java/com/clean/code/checkout/CheckoutSystem.java
+++ src/main/java/com/clean/code/checkout/CheckoutSystem.java
@@ -11,12 +11,21 @@ public class CheckoutSystem {
 		Map<String, Double> itemPriceMap = new HashMap<>();
 		itemPriceMap.put("A", Double.valueOf(50.0));
 		itemPriceMap.put("B", Double.valueOf(30.0));
+		int countofItemA = 0;
+		Double itemADiscountedPrice = Double.valueOf(130.0);
 		
-		if(itemsForBillingAtCheckout != null){
+		if(itemsForBillingAtCheckout != null){			
 			for(String item : itemsForBillingAtCheckout){
-				totalItemPrice += itemPriceMap.getOrDefault(item, totalItemPrice);
-			}			
-		}			
+				if("A".equals(item)){
+					countofItemA++;
+				}else{
+					totalItemPrice += itemPriceMap.getOrDefault(item, totalItemPrice);
+				}				
+			}
+			int numberOf3ItemAs = countofItemA/3;
+			int numberOfRemainingItemA = countofItemA % 3;
+			totalItemPrice += itemADiscountedPrice * numberOf3ItemAs + numberOfRemainingItemA * itemPriceMap.get("A"); 
+		}		
 		return totalItemPrice;
 	}
 
