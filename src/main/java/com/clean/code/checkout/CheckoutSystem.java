19e2b164bc3a4347d674f77165c383536f717035
Puthran, Harish <Harish.Puthran@in.tesco.com>
2016-11-30 10:52:19 +0000

    Commit 7 - REFACTOR (Refactored the implementation by using Map : Item-Price Mapping - A & B Items)

--- src/main/java/com/clean/code/checkout/CheckoutSystem.java
+++ src/main/java/com/clean/code/checkout/CheckoutSystem.java
@@ -1,17 +1,21 @@
 package com.clean.code.checkout;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CheckoutSystem {
 
 	public double calculateTotatlPrice(List<String> itemsForBillingAtCheckout) {
-		if(itemsForBillingAtCheckout != null && itemsForBillingAtCheckout.size() == 1 && itemsForBillingAtCheckout.contains("A")){
-			return 50.0d;
-		}
-		if(itemsForBillingAtCheckout != null && itemsForBillingAtCheckout.size() == 1 && itemsForBillingAtCheckout.contains("B")){
-			return 30.0d;
-		}		
-		return 0.0;
+		Double zeroPrice = Double.valueOf(0.0);
+		Map<String, Double> itemPriceMap = new HashMap<>();
+		itemPriceMap.put("A", Double.valueOf(50.0));
+		itemPriceMap.put("B", Double.valueOf(30.0));
+		
+		if(itemsForBillingAtCheckout != null && itemsForBillingAtCheckout.size() == 1){
+			return itemPriceMap.getOrDefault(itemsForBillingAtCheckout.get(0), zeroPrice);			
+		}			
+		return zeroPrice;
 	}
 
 }
